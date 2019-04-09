87f3e7e85cd5bce41caf4d97a1ebb22205112217
Elizabeth Hand <eh919353@wtstudents.wtamu.edu>
2019-04-09 12:10:00 -0500

    4.9.19

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -13,10 +13,40 @@ namespace HandLab11.Pages
         private readonly ProfessorDbContext _context;
         public List<Professor> Professors {get; set;}
         
+        
+
         public IndexModel(ProfessorDbContext context)
         {
             _context = context;
         }
+
+        [BindProperty(SupportsGet = true)]
+        public int PageNum {get; set;} = 1;
+
+        public int PageSize {get; set;} = 10;
+
+        public string namesort {get; set;}
+
+        public string CurrentFilter {get; set;}
+
+        public string CurrentSort {get; set;}
+
+        
+        public async Task OnGetAsync(string sortOrder)
+        {
+            var query = _context.Professor.Select(p =>p);
+
+            namesort = String.IsNullOrEmpty(sortOrder) ? "name_desc" : "";
+
+            query = query.OrderByDesc(p => p.FirstName);
+        }
+        public async Task OnGetAsync(string sortOrder, string CurrentFilter, string searchString, int? pageIndex)
+        {
+            CurrentSort = sortOrder;
+            namesort = String
+            skip((PageNum - 1)*PageSize).Take(PageSize).ToListAs
+        }
+
         public void OnGet()
         {
             Professors = _context.Professor.ToList();
