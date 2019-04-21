f824f57543c40ba186c9de21d49539ca7debc94d
bhand91 <43619765+bhand91@users.noreply.github.com>
2019-04-21 15:01:52 -0500

    4.21.19

--- Pages/Professor.cshtml.cs
+++ Pages/Professor.cshtml.cs
@@ -7,36 +7,69 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using HandLab11.Models;
 using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.Extensions.Logging;
 
 namespace HandLab11.Pages
 {
     public class ProfessorModel : PageModel
     {
-        private readonly ProfessorDbContext _context;
-        public List<Professor> Professors {get; set;}
-        public SelectList ProfessorDropDown {get; set;}
-
-        public ProfessorModel(ProfessorDbContext context)
+        private readonly HandLab11.Models.ProfessorDbContext _context;
+        
+        private readonly ILogger _logger;
+        public ProfessorModel(HandLab11.Models.ProfessorDbContext context, ILogger<ProfessorModel> logger)
         {
+            _logger = logger;
             _context = context;
         }
+        public PaginatedList<Professor> Professors {get; set;}
+        
+        [BindProperty(SupportsGet = true)]
+        public string SearchString {get; set;}
+        public SelectList ProfessorDropDown {get; set;}
 
+        public SelectList SortList {get; set;}
+        
+        [BindProperty(SupportsGet = true)]
+        public string Prof {get; set;}
         public string NameSort {get; set;}
         public string CurrentFilter {get; set;}
-
         public string CurrentSort {get; set;}
-        public async Task OnGetAsync(string sortOrder, string searchString)
+        public async Task OnGetAsync(string sortOrder, string searchString, string currentFilter, int? pageIndex)
         {
+            CurrentSort = sortOrder;
             NameSort = String.IsNullOrEmpty(sortOrder) ? "name_desc" : "";
+            
+            if(searchString != null)
+            {
+                pageIndex = 1;
+            }
+            else
+            {
+                searchString = currentFilter;
+            }
+
             CurrentFilter = searchString;
+
+            var professors = _context.Professor.Include(p => p.Courses).Select( p => new {Display = $"{p.FirstName} {p.LastName}"});
+            //.Select(p => p);
+            
+            var courses = _context.Course.Include(c => c.Professor).Select(c => c);
+
+            if(!string.IsNullOrEmpty(SearchString))
+            {
+                courses = courses.Where(c => c.Description.ToUpper().Contains(searchString));               
+            }
 
             IQueryable<Professor> professorQuery = from p in _context.Professor
                                                     select p;
 
-            if(!String.IsNullOrEmpty(searchString))
+            if(!string.IsNullOrEmpty(Prof))
             {
-                professorQuery = professorQuery.Where(s => s.LastName.ToUpper().Contains(searchString.ToUpper()) || s.FirstName.ToUpper().Contains(searchString.ToUpper()));
+                professors = professors.Where(x => x.Display == Prof);
             }
+
+            ProfessorDropDown = new SelectList (await professors.Distinct().ToListAsync());
+            
             switch (sortOrder)
             {
                 case "name_desc":
@@ -46,17 +79,22 @@ namespace HandLab11.Pages
                     professorQuery = professorQuery.OrderBy(p => p.LastName);
                     break;
             }
-
-            Professors = await professorQuery.AsNoTracking().ToListAsync();
+            
+            //Professors = await professorQuery.AsNoTracking().ToListAsync();
 
             // Get list of Professors
-            Professors = _context.Professor.Include(p => p.Courses).ToList();
+            //Professors =  _context.Professor.Include(p => p.Courses).ToList();
             // Create drop down list from Blogs
             // "BlogId" is Blog Model property to use as the value= setting
             // "Title" is the property to display in the drop down list
             // 2 is OPTIONAL. It says the default selected blog should be 2.
             // Change that or delete it and see what happens.
-            ProfessorDropDown = new SelectList(Professors, "ProfessorId", "LastName");
+
+            int pageSize = 10;
+
+            Professors = await PaginatedList<Professor>.CreateAsync(
+                professorQuery.AsNoTracking(), pageIndex ?? 1, pageSize);
+        
         }
     }
 }
